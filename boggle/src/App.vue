e519c8c51d4bdca04f76eb597b0e7b03777dbb45
simjust175 <bunim175@gmail.com>
2023-09-28 21:39:24 +0300

    First commit

---
+++ boggle/src/App.vue
@@ -0,0 +1,110 @@
+<template>
+  <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  <div class="mainContainer">
+    <div class="boggle-board boggleItems">
+     </div>
+    <div class="timerArea">
+      <BoggleTimer />
+    </div>
+  </div>
+</template>
+
+<script>
+import BoggleTimer from "./components/BoggleTimer.vue"
+export default {
+  components:{
+    BoggleTimer,
+  }
+
+}
+</script>
+
+<style>
+:root{
+  --color-primary: #42b883;
+  --color-secondary-dark: #388f68;
+  --border-radius: 8px;
+}
+  .boggleItems{
+    border-radius: var(--border-radius);
+
+  }
+
+/* Boggle Board */
+.boggle-board {
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  gap: 10px;
+  background-color: #42b883;
+  padding: 10px;
+  border-radius: 10px;
+  perspective: 800px;
+}
+
+/* Boggle Cell */
+.boggle-cell {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 24px;
+  font-weight: bold;
+  background-color: #ffffff;
+  border-radius: 5px;
+  cursor: pointer;
+  transform-style: preserve-3d;
+  transition: transform 0.3s ease;
+  position: relative;
+  box-shadow: 0px 3px 6px rgba(0, 0, 0, 0.16), 0px 3px 6px rgba(0, 0, 0, 0.23);
+}
+
+.boggle-cell::before {
+  content: "";
+  position: absolute;
+  top: -5px;
+  left: -5px;
+  right: -5px;
+  bottom: -5px;
+  background-color: #ffffff;
+  border-radius: 5px;
+  transform: translateZ(-5px);
+  box-shadow: 0px 0px 16px 0px rgba(0, 0, 0, 0.2);
+}
+
+.boggle-cell::after {
+  content: "";
+  position: absolute;
+  top: -5px;
+  left: -5px;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.1);
+  border-radius: 5px;
+  transform: translateY(5px);
+  transition: transform 0.3s ease;
+  box-shadow: 0px 0px 16px 0px rgba(0, 0, 0, 0.2);
+}
+
+.boggle-cell:hover {
+  transform: rotateX(10deg) rotateY(-10deg) translateZ(10px) scale(1.05);
+  box-shadow: 0px 6px 12px rgba(0, 0, 0, 0.16), 0px 6px 12px rgba(0, 0, 0, 0.23);
+}
+
+.boggle-cell.active {
+  background-color: #ffd700;
+  color: #ffffff;
+  transform: rotateX(10deg) rotateY(-10deg) translateZ(10px) scale(1.05);
+  box-shadow: 0px 6px 12px rgba(0, 0, 0, 0.16), 0px 6px 12px rgba(0, 0, 0, 0.23);
+}
+
+/* Example Usage */
+.boggle-board {
+  width: 400px;
+  height: 400px;
+}
+
+.boggle-cell {
+  width: 80px;
+  height: 80px;
+}
+
+</style>
---
+++ boggle/src/components/BoggleTimer.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="timerArea">
+    <div class="boggleItems timerItems timerBody">
+        <div class="boggleItems timerItems timerProgress ">
+            <input type="number" v-model="timer" placeholder="30">
+            {{ timer }}
+        </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    name: "BoggleTimer",
+    data(){
+      return{
+        timerRange: 30,
+      }
+    }
+
+
+}
+</script>
+
+<style>
+  .timerItems{
+    height: 80px;
+    width: 150px;
+    border: 1px solid #42b883;
+  }
+  ::input{
+    border: none;
+    outline: none;
+  }
+</style>
